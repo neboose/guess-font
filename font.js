2736579d1358669d61a5a928b1c09c6935d7cb8b
neboose <ben.klump@gmail.com>
2025-08-21 15:10:00 +0000

    feat: added changing fonts

--- font.js
+++ font.js
@@ -1,4 +1,4 @@
-fonts = [
+const quizFonts = [
     "Times Roman",
     "Arial",
     "Verdana"
@@ -9,4 +9,5 @@ document.addEventListener("DOMContentLoaded", function() {
     /**@type {HTMLElement} */
     let prompt = document.querySelector("#prompt");
     let form = document.querySelector("form");
+    prompt.style.fontFamily = quizFonts[Math.floor(Math.random() * quizFonts.length)];
 });
