e4f6063bea89a4b491a4cb963c1d88c383542f71
neboose <ben.klump@gmail.com>
2025-08-21 23:56:50 +0000

    fix: updated the display on the next event listener

--- font.js
+++ font.js
@@ -52,6 +52,6 @@ document.addEventListener("DOMContentLoaded", function() {
         prompt.style.fontFamily = quizFonts[fontNumber];
         fontInput.value = "";
         result.innerHTML = "";
-        next.style.hidden = true;
+        next.style.display = "hidden";
     });
 });
