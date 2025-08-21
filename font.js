1b54760259e6ca1f88828b7ef1097e27b157f579
neboose <ben.klump@gmail.com>
2025-08-21 23:12:05 +0000

    feat: added form event

--- font.js
+++ font.js
@@ -9,5 +9,9 @@ document.addEventListener("DOMContentLoaded", function() {
     /**@type {HTMLElement} */
     let prompt = document.querySelector("#prompt");
     let form = document.querySelector("form");
+    let result = document.querySelector("#result");
     prompt.style.fontFamily = quizFonts[Math.floor(Math.random() * quizFonts.length)];
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+    });
 });
