a97975ffc2dea0798ca9a40c89735837f42d9ca3
neboose <ben.klump@gmail.com>
2025-08-21 03:28:56 +0000

    feat: added simple script for changing font

---
+++ font.js
@@ -0,0 +1,10 @@
+fonts = [
+    "Times Roman",
+    "Arial",
+    "Verdana"
+]
+
+document.addEventListener("DOMContentLoaded", function() {
+    let prompt = document.querySelector("#prompt");
+    let form = document.querySelector("form");
+});
