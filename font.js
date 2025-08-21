ff98e7d54f37f21513725aa4d896207be6334a42
neboose <ben.klump@gmail.com>
2025-08-21 23:28:11 +0000

    feat: added basic logic for correct/incorrect

--- font.js
+++ font.js
@@ -10,8 +10,26 @@ document.addEventListener("DOMContentLoaded", function() {
     let prompt = document.querySelector("#prompt");
     let form = document.querySelector("form");
     let result = document.querySelector("#result");
-    prompt.style.fontFamily = quizFonts[Math.floor(Math.random() * quizFonts.length)];
+
+    /**@type {HTMLInputElement} */
+    let fontInput = document.querySelector("#font-input");
+    fontNumber = Math.floor(Math.random() * quizFonts.length)
+    prompt.style.fontFamily = quizFonts[fontNumber];
     form.addEventListener("submit", function(event) {
+        if (!fontInput.value)
+        {
+            result.innerHTML = "Please type an answer."
+        }
+        else if (fontInput.innerHTML == quizFonts[fontNumber])
+        {
+            result.style.color = "green";
+            result.innerHTML = "Correct!"
+        }
+        else
+        {
+            result.style.color = "red";
+            result.innerHTML = "Incorrect";
+        }
         event.preventDefault();
     });
 });
